c1ab59141d06beb93f558a079bd2a4fe46854757
sunxn1991@163.com <sunxn1991@163.com>
2019-12-05 16:03:41 +0800

    javaweb目录下增加sso\sessioncookie

---
+++ javaweb/com_live_test_javaweb_sso/src/main/java/com/live/test/javaweb/sso/App.java
@@ -0,0 +1,13 @@
+package com.live.test.javaweb.sso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
