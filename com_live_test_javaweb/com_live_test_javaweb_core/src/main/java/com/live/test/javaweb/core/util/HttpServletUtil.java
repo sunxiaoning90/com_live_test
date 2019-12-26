1b1de8a7274384249408ae82b90e05356f9953e5
sunxn1991@163.com <sunxn1991@163.com>
2019-12-26 18:03:32 +0800

    判断是否请求的静态资源

--- com_live_test_javaweb/com_live_test_javaweb_core/src/main/java/com/live/test/javaweb/core/util/HttpServletUtil.java
+++ com_live_test_javaweb/com_live_test_javaweb_core/src/main/java/com/live/test/javaweb/core/util/HttpServletUtil.java
@@ -87,6 +87,6 @@ public class HttpServletUtil {
 	 */
 	public static boolean isStaticResource(HttpServletRequest request) {
 		String uri = request.getRequestURI();
-		return uri.indexOf(".css") > -1 || uri.indexOf(".js")> -1 || uri.indexOf(".png") > -1;
+		return uri.lastIndexOf(".css") > -1 || uri.indexOf(".js")> -1 || uri.indexOf(".png") > -1;
 	}
 }
