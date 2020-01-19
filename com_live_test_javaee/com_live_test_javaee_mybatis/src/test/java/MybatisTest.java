3c423b7bf9f43a287b4604c86e1547c7d5272f94
sunxn1991@163.com <sunxn1991@163.com>
2020-01-19 15:57:29 +0800

    mybatis 简单封装了一个mybatis工具类,MyBatisHelper,实现增删改查

---
+++ com_live_test_javaee/com_live_test_javaee_mybatis/src/test/java/MybatisTest.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.live.test.javaee.mybatis.mapper.EvaluateMapper;
+import com.live.test.javaee.mybatis.po.Evaluate;
+
+import junit.framework.TestCase;
+
+public class MybatisTest extends TestCase {
+
+	@Test
+	public void test1() {
+		String mybatisConfig = "mybatis-config.xml";
+		InputStream in = null;
+		try {
+			in = Resources.getResourceAsStream(mybatisConfig);
+			System.out.println("in:" + in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
+		System.out.println("sqlSessionFactory:" + sqlSessionFactory);
+
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		System.out.println("sqlSession:" + sqlSession);
+		
+		EvaluateMapper mapper = sqlSession.getMapper(EvaluateMapper.class);
+		
+		int id = 1;
+		Evaluate evaluate = mapper.getEvaluateById(id);
+		System.out.println("evaluate:"+evaluate);
+		
+		List<Evaluate> evaluateList = mapper.getEvaluateByAll();
+		for (int i = 0; i < evaluateList.size(); i++) {
+			Evaluate e = evaluateList.get(i);
+			System.out.println("evaluate:"+e);
+		}
+		
+		int i = mapper.saveEvaluate(new Evaluate(20, "a", "b"));
+		sqlSession.commit();
+		System.out.println("save:"+i);
+		
+	}
+}
---
+++ com_live_test_javaee/com_live_test_javaee_mybatis/src/test/java/com/live/test/javaee/mybatis/EvaluateServiceTest.java
@@ -0,0 +1,33 @@
+package com.live.test.javaee.mybatis;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.live.test.javaee.mybatis.po.Evaluate;
+import com.live.test.javaee.mybatis.service.EvaluateService;
+
+public class EvaluateServiceTest {
+
+	@Test
+	public void getEvaluateById() {
+		int id = 2;
+		Evaluate po = new EvaluateService().getEvaluateById(id);
+		System.out.println(po);
+	}
+	
+	@Test
+	public void getEvaluateAll() {
+		List<Evaluate> list = new EvaluateService().getEvaluateAll();
+		for (Evaluate evaluate : list) {
+			System.out.println(evaluate);
+		}
+	}
+	
+	@Test
+	public void saveEvaluate() {
+		Evaluate po = new Evaluate(101, "非常满意", "...");
+		boolean flg = new EvaluateService().saveEvaluate(po);
+			System.out.println(flg);
+	}
+}
