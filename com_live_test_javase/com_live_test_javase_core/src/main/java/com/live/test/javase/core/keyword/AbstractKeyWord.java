c393e4adf6e9fb7003560f975bee27d6bc5c8ea8
sunxn1991@163.com <sunxn1991@163.com>
2020-05-26 11:52:53 +0800

    迁移com_live_test_javase_core 致 com_live_test_api_core

--- com_live_test_javase/com_live_test_javase_core/src/main/java/com/live/test/javase/core/keyword/AbstractKeyWord.java
+++
@@ -1,8 +0,0 @@
-package com.live.test.javase.core.keyword;
-
-public abstract class AbstractKeyWord {
-	abstract void a();
-//	static abstract void b();
-//	native abstract void c();
-//	synchronized abstract void d();
-}
