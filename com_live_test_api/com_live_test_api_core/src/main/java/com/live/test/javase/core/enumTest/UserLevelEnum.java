a7c02697526acd6e35ce7aeddb35d947d81cbdaa
sunxn1991@163.com <sunxn1991@163.com>
2020-06-18 18:20:10 +0800

    枚举类，1）private 构造 2）重写toString ()

--- com_live_test_api/com_live_test_api_core/src/main/java/com/live/test/javase/core/enumTest/UserLevelEnum.java
+++ com_live_test_api/com_live_test_api_core/src/main/java/com/live/test/javase/core/enumTest/UserLevelEnum.java
@@ -1,11 +1,26 @@
 package com.live.test.javase.core.enumTest;
 
 public enum UserLevelEnum {
-	Normal("normal"),Vip("vip");
-	
-	String type;
-	
-	UserLevelEnum(String type){
+	Normal("normal"), Vip("vip");
+
+	private String type;
+
+//	public UserLevelEnum(String type){ //只允许为private，不允许为public Illegal modifier for the enum constructor; only private is permitted.
+	UserLevelEnum(String type) {
 		this.type = type;
 	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	/**
+	 * 可以重写toString()
+	 */
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
 }
