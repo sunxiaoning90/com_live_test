74e87bcb25ee38bcb1f6c6f62f9b7a8275aef685
sunxn1991@163.com <sunxn1991@163.com>
2020-07-14 15:00:11 +0800

    Dubbo 开发笔记

---
+++ com_live_test_distributed/com_live_test_distributed_dubbo/src/main/java/com/live/test/javaee/spring/service/EvaluateService.java
@@ -0,0 +1,9 @@
+package com.live.test.javaee.spring.service;
+
+import com.live.test.javaee.spring.po.Evaluate;
+
+public class EvaluateService {
+	public Evaluate getEvaluateById(int id) {
+		return null;
+	}
+}
