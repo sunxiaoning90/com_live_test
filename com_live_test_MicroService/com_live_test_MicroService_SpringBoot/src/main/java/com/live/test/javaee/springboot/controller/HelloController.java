adbe347d9ae2318f898e5977e7025ad6b20113a9
sunxn1991@163.com <sunxn1991@163.com>
2020-08-05 17:34:35 +0800

    com_live_test_MicroService_SpringBoot

--- com_live_test_MicroService/com_live_test_MicroService_SpringBoot/src/main/java/com/live/test/javaee/springboot/controller/HelloController.java
+++ com_live_test_MicroService/com_live_test_MicroService_SpringBoot/src/main/java/com/live/test/javaee/springboot/controller/HelloController.java
@@ -1,17 +1,17 @@
 package com.live.test.javaee.springboot.controller;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ComponentScan
+@Component
 @Controller
 public class HelloController {
 
 	@RequestMapping("/hello")
 	@ResponseBody
 	public String hello() {
-		return "test";
+		return "hello l";
 	}
 }
