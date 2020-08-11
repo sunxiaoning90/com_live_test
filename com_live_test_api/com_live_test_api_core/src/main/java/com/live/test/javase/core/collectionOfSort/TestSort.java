8ac63a2c1f05e668e0d7da1fd83760be11885654
sunxn1991@163.com <sunxn1991@163.com>
2020-08-11 16:27:54 +0800

    /** 	 * 获取百分比 	 * 	 * @param a         : 被除数 	 * @param b         : 除数 	 * @param minDigits : 最小小数位 	 * @param maxDigits ： 最大 小数位 	 * @return 	 */

---
+++ com_live_test_api/com_live_test_api_core/src/main/java/com/live/test/javase/core/collectionOfSort/TestSort.java
@@ -0,0 +1,18 @@
+package com.live.test.javase.core.collectionOfSort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestSort {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		
+		list.add("a");
+		list.add("c");
+		list.add("b");
+		
+		System.out.println(list);
+	}
+
+}
---
+++ com_live_test_api/com_live_test_api_core/src/main/java/com/live/test/javase/core/jmath/DecimalsUtil.java
@@ -0,0 +1,49 @@
+package com.live.test.javase.core.jmath;
+
+import java.text.NumberFormat;
+
+public class DecimalsUtil {
+
+	public static void main(String[] args) {
+		int sum = 3;
+		int a = 1;
+		String percent = getPercent(a, sum, 2);
+		System.out.println(percent);
+	}
+
+	/**
+	 * 获取百分比
+	 * 
+	 * @param a      : 被除数
+	 * @param b      : 除数
+	 * @param digits : 小数位
+	 * @return
+	 */
+	public static String getPercent(int a, int b, int digits) {
+		return getPercent(a, b, digits, digits);
+	}
+
+	/**
+	 * 获取百分比
+	 * 
+	 * @param a         : 被除数
+	 * @param b         : 除数
+	 * @param minDigits : 最小小数位
+	 * @param maxDigits ： 最大 小数位
+	 * @return
+	 */
+	public static String getPercent(int a, int b, int minDigits, int maxDigits) {
+		String percent = "0.00%";
+		if (a != 0) {
+			NumberFormat numberFormat = NumberFormat.getInstance();
+			// numberFormat.setMaximumIntegerDigits(n);//
+			numberFormat.setMaximumFractionDigits(minDigits);
+			numberFormat.setMinimumFractionDigits(maxDigits);
+			percent = numberFormat.format((float) a / (float) b * 100) + "%";
+//			percent = numberFormat.format((double) a / (double) b * 100) + "%";
+		}
+
+		return percent;
+	}
+
+}
