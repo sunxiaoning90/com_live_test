020956d218d1b915bc0529a7397ef02014eb362c
sunxn1991@163.com <sunxn1991@163.com>
2020-10-26 11:42:42 +0800

    // RestTemplate 缺点：使用 RestTemplate 进行http调用时，服务地址是固定的，虽然可以指定域名，可以修改ngxin的配置文件来修改真实服务器地址，但是还是很繁琐不方便 		String serverAddress = "http://192.168.1.50:8082"; 		String url = serverAddress + "/discovery/provider/echo/" + str; 		return restTemplate.getForObject(url, String.class);

--- com_live_test_MicroService/com_live_test_MicroService_SpringCloudNetflix/com_live_test_MicroService_SpringCloudNetflix_eureka/com_live_test_MicroService_SpringCloudNetflix_eureka_discovery_consumer/src/main/java/com/live/test/javaee/springboot/discovery/controller/DiscoveryConsumerController.java
+++ com_live_test_MicroService/com_live_test_MicroService_SpringCloudNetflix/com_live_test_MicroService_SpringCloudNetflix_eureka/com_live_test_MicroService_SpringCloudNetflix_eureka_discovery_consumer/src/main/java/com/live/test/javaee/springboot/discovery/controller/DiscoveryConsumerController.java
@@ -23,20 +23,23 @@ import com.netflix.discovery.EurekaClient;
 public class DiscoveryConsumerController {
 
 	@Bean
-	@LoadBalanced
+//	@LoadBalanced
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		RestTemplate rt = new RestTemplate();
+		System.out.println(rt);
+		return rt;
 	}
 
 	@Autowired
 	public RestTemplate restTemplate;
-	
+
 	@RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
 	public String echo(@PathVariable String str) {
 		System.out.println(str);
-//		return "hello" + str;
-//		String url = "http://localhost:8082/"+"discovery/provider/echo/"+str;
-		String url = "http://192.168.1.50:8082/"+"discovery/provider/echo/"+str;
+
+		// RestTemplate 缺点：使用 RestTemplate 进行http调用时，服务地址是固定的，虽然可以指定域名，可以修改ngxin的配置文件来修改真实服务器地址，但是还是很繁琐不方便
+		String serverAddress = "http://192.168.1.50:8082";
+		String url = serverAddress + "/discovery/provider/echo/" + str;
 		return restTemplate.getForObject(url, String.class);
 	}
 
