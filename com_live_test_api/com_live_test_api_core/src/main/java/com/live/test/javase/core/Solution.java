c3c781fd4b1aa40d6262e1230f1a145c54dc37fb
sunxn1991@163.com <sunxn1991@163.com>
2020-11-06 11:14:24 +0800

    目录整理

---
+++ com_live_test_api/com_live_test_api_core/src/main/java/com/live/test/javase/core/Solution.java
@@ -0,0 +1,26 @@
+package com.live.test.javase.core;
+
+public class Solution {
+	public static int[] twoSum(int[] z, int target) {
+		int[] a = new int[2];
+		for (int n = 0; n < z.length; n++) {
+			int x = z[n];
+			for (int i = n + 1; i < z.length; i++) {
+				int h = x + z[i];
+				if (target == h) {
+					a[0] = n;
+					a[1] = i;
+				}
+			}
+		}
+		return a;
+	}
+
+	public static void main(String[] args) {
+		int[] z = new int[] { 1, 2, 8, 9 };
+		int target = 9;
+		int[] r = twoSum(z, target);
+
+		System.out.println(r[0] + " " + r[1]);
+	}
+}
