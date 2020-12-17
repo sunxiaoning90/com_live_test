6ba320fb5a931b39c3eff6d61b8f4d5ce39a21fe
sunxn1991@163.com <sunxn1991@163.com>
2020-12-17 18:18:35 +0800

    补充工具类

---
+++ com_live_test_api/com_live_test_api_core/src/main/java/com/live/test/javase/core/collection/testList/Persion.java
@@ -0,0 +1,13 @@
+package com.live.test.javase.core.collection.testList;
+
+public class Persion {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
